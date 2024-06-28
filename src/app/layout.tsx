3a46df2676d1242592e6024a507edc335fde1f9f
H <hatim.v@hotmail.co.uk>
2024-06-28 14:11:26 +0000

    Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ import '@/styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: 'CC',
+  description: 'Comparison Site',
 }
 
 export default function RootLayout({
